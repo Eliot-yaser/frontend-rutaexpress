164af149190f9455369b9e720c17d36e1ae69db1
Eliot-yaser <el.armijo@duocuc.cl>
2026-09-15 09:08:06 -0300

    Actualizar variables de entorno en environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,17 @@
 export const environment = {
-  production: false,
-  azure: {
-    clientId: 'c515cc67-aed4-4210-89f0-433271797592',
-    authority: 'https://login.microsoftonline.com/d4e45c2c-5598-4953-9517-f1c5f09d4eb1',
-    redirectUri: 'http://localhost:4200'
-  }
+
+ production: false,
+
+ apiUrl: 'http://localhost:8080', // Puerto de tu ms-rutaexpress-bff
+
+ azure: {
+
+  clientId: 'c515cc67-aed4-4210-89f0-433271797592',
+
+  authority: 'https://login.microsoftonline.com/d4e45c2c-5598-4953-9517-f1c5f09d4eb1',
+
+  redirectUri: 'http://localhost:4200'
+
+ }
+
 };
